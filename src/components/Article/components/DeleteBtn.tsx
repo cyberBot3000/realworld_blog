156e56cfdb27dead130c943c69fbc2816769d651
znicrasovz0@gmail.com <znicrasovz0@gmail.com>
2023-05-30 16:03:47 +0800

    add delete btn popup

--- src/components/Article/components/DeleteBtn.tsx
+++ src/components/Article/components/DeleteBtn.tsx
@@ -1,28 +1,61 @@
-import React, { useEffect } from 'react';
-import { useArticleContext } from '../hooks';
-import { useSendDeleteArticleMutation } from 'service';
+import { MouseEventHandler, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSendDeleteArticleMutation } from 'service';
+import { useOutsideClick } from 'utils/hooks';
+import { useArticleContext } from '../hooks';
 
 export const DeleteBtn = () => {
 	const { slug } = useArticleContext();
 	const [sendDeleteArticle, { isSuccess }] = useSendDeleteArticleMutation();
 	const navigate = useNavigate();
+	const [popupActive, setIsPopupActive] = useState(false);
+	const popupRef = useRef<HTMLDivElement | null>(null);
+
+	const hidePopup = () => {
+		setIsPopupActive(false);
+	};
+
+	const deleteBtnClickHandler: MouseEventHandler<HTMLButtonElement> = e => {
+		e.stopPropagation();
+		if (popupActive) return;
+		setIsPopupActive(true);
+	};
+
+	const delteArticle = () => {
+		sendDeleteArticle({ slug });
+		setIsPopupActive(false);
+	};
+
+	useOutsideClick<HTMLDivElement>(popupRef, hidePopup);
 
 	useEffect(() => {
 		if (isSuccess) navigate('/');
 	}, [isSuccess]);
-
 	return (
-		<button
-			className="article__action-btn article__action-btn_warning"
-			onClick={() => {
-				sendDeleteArticle({
-					slug,
-				});
-			}}
-		>
-			Delete
-		</button>
+		<div className="article__popup-btn-block">
+			<button className="article__action-btn article__action-btn_warning" onClick={deleteBtnClickHandler}>
+				Delete
+			</button>
+			{popupActive && (
+				<div className="article__btn-popup" ref={popupRef}>
+					<div className="article__text">are you sure to delete this article?</div>
+					<div className="article__btn-popup-actions">
+						<button
+							className="article__action-btn article__action-btn_sm article__action-btn_default"
+							onClick={hidePopup}
+						>
+							No
+						</button>
+						<button
+							className="article__action-btn article__action-btn_sm article__action-btn_accent"
+							onClick={delteArticle}
+						>
+							Yes
+						</button>
+					</div>
+				</div>
+			)}
+		</div>
 	);
 };
 
--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,6 +1,21 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import type { TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch } from 'store';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from 'store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useOutsideClick = <T extends HTMLElement>(ref: React.RefObject<T>, onClick: (e: MouseEvent) => void) => {
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent): void => {
+			if (ref.current && !ref.current.contains(event.target as Node)) {
+				onClick(event);
+			}
+		};
+		document.addEventListener('click', handleClickOutside);
+		return () => {
+			document.removeEventListener('click', handleClickOutside);
+		};
+	}, [ref]);
+};
